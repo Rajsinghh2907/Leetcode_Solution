4cde99d8ac7292390e8a6c942bf69205afd34367
Raj Kumar Singh <rajkumarshivam2054@gmail.com>
2024-07-01 22:28:29 +0530

    Time: 1 ms (100%), Space: 42.2 MB (51.03%) - LeetHub

--- 2595-number-of-even-and-odd-bits/2595-number-of-even-and-odd-bits.java
+++ 2595-number-of-even-and-odd-bits/2595-number-of-even-and-odd-bits.java
@@ -1,12 +1,13 @@
 class Solution {
     public int[] evenOddBit(int n) {
         
-        String str = Integer.toBinaryString(n);
+        String str = Integer.toBinaryString(n); // It convert the Num into BinaryString
 
         int odd = 0, even = 0;
 
+
         for (int i = 0; i < str.length(); i++) {
-            // Check if the bit is '1'
+           
             if (str.charAt(str.length() - 1 - i) == '1') {
                 if (i % 2 == 0) {
                     even++;
