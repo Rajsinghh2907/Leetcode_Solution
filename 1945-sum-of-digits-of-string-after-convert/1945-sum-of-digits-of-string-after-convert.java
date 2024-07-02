895c92d0d3d4693d6cc6b9dc37a6f7ba17beea75
Raj Kumar Singh <rajkumarshivam2054@gmail.com>
2024-07-02 23:01:22 +0530

    Time: 1 ms (92.03%), Space: 41.4 MB (78.41%) - LeetHub

---
+++ 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int getLucky(String s, int k) {
+        
+        StringBuilder numStr = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            numStr.append(c - 'a' + 1);
+            // The given string s is converted into a numerical string where each 
+            // character c is replaced by its position in the alphabet 
+            // (i.e., 'a' -> 1, 'b' -> 2, ..., 'z' -> 26).
+        }
+
+        int sum = 0;
+
+        for(char ch : numStr.toString().toCharArray()){
+            sum += ch - '0';
+        }
+
+        for(int i = 1; i<k; i++){
+            int newSum = 0;
+
+            while(sum  > 0){
+                newSum += sum % 10;
+                sum /= 10;
+            }
+
+            sum = newSum;
+            
+        }
+
+        return sum;
+    }
+}
