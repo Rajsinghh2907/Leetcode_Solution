92f664c4b07f46c36b1c2ef239d94c204e010dc9
Raj Kumar Singh <rajkumarshivam2054@gmail.com>
2024-07-03 23:31:21 +0530

    Time: 0 ms (100%), Space: 39.9 MB (91.98%) - LeetHub

--- 0343-integer-break/0343-integer-break.java
+++ 0343-integer-break/0343-integer-break.java
@@ -1,6 +1,13 @@
 class Solution {
     public int integerBreak(int n) {
 
+        // for this question if we check the example we saw that if n is greater than 
+        // 4 then ans is multiple of 3 and 2nd max answer is multiple of 2 but we need
+        // Max number so multiply of 3 is used 
+
+        // if (n <=3)
+        // for n = 2,the only possible split is 1 + 1,which gives a product of 1.
+        // for n = 3,the only possible split is 2 + 1, which gives a product of 2.
         if(n < 4){
             return n-1;
         }
