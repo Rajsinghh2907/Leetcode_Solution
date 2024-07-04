92ef4484917a05db11e95ccdeb1c345482789536
Raj Kumar Singh <rajkumarshivam2054@gmail.com>
2024-07-04 09:31:08 +0530

    Time: 5 ms (84.77%), Space: 83.3 MB (27.56%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
@@ -11,22 +11,46 @@
 class Solution {
     public ListNode mergeNodes(ListNode head) {
         
-        ListNode temp = head;
+        // ListNode temp = head;
 
-        while(temp != null && temp.next!= null){
-            if(temp.next.val != 0){
-                int sum = temp.val + temp.next.val;
-                temp.val = sum;
-                temp.next = temp.next.next;
+        // while(temp != null && temp.next!= null){
+        //     if(temp.next.val != 0){
+        //         int sum = temp.val + temp.next.val;
+        //         temp.val = sum;
+        //         temp.next = temp.next.next;
 
 
-            } else if(temp.next.val == 0 && temp.next.next == null){
-                temp.next = null;
-            } else{
-                temp = temp.next;
+        //     } else if(temp.next.val == 0 && temp.next.next == null){
+        //         temp.next = null;
+        //     } else{
+        //         temp = temp.next;
+        //     }
+        // }
+
+        // return head;
+
+
+
+
+
+        ListNode New = new ListNode(0);
+        ListNode curr = New;
+        
+        ListNode temp = head.next; // Skip the first 0
+        int sum = 0;
+
+        while (temp != null) {
+            if (temp.val == 0) {
+                // When encountering a 0, create a new node with the sum
+                curr.next = new ListNode(sum);
+                curr = curr.next;
+                sum = 0; // Reset the sum
+            } else {
+                sum += temp.val; // Add value to the sum
             }
+            temp = temp.next;
         }
-
-        return head;
+        
+        return New.next; // Skip the dummy node and return the actual head
     }
 }
