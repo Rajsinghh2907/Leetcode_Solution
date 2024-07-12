88d2a7c793e929d7b1b2e36e606e5ec7a3cb1f88
Raj Kumar Singh <rajkumarshivam2054@gmail.com>
2024-07-12 22:39:19 +0530

    Time: 0 ms (100%), Space: 41.4 MB (48.97%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -2,5 +2,30 @@ class Solution {
     public boolean stoneGame(int[] piles) {
         
         return true; // if it is even then always the first will win
+
+
+
+
+
+
+
+
+
+
+        // int evenSum = 0;
+        // int oddSum = 0;
+        
+        // // Calculate the sum of stones in even-indexed and odd-indexed piles
+        // for (int i = 0; i < piles.length; i++) {
+        //     if (i % 2 == 0) {
+        //         evenSum += piles[i];
+        //     } else {
+        //         oddSum += piles[i];
+        //     }
+        // }
+        
+        // // Alice can always choose the set (even-indexed or odd-indexed) with the higher sum
+        // return true; // Alice always wins by choosing the optimal set
+
     }
 }
