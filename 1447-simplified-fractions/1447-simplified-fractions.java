d31075ec3cc300f6b45bcfb5f526027e565a8649
Raj Kumar Singh <rajkumarshivam2054@gmail.com>
2024-07-13 22:49:53 +0530

    Time: 18 ms (85.44%), Space: 45.8 MB (9.49%) - LeetHub

---
+++ 1447-simplified-fractions/1447-simplified-fractions.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<String> simplifiedFractions(int n) {
+     List<String> result = new ArrayList<>();
+        for (int denominator = 2; denominator <= n; denominator++) {
+            for (int numerator = 1; numerator < denominator; numerator++) {
+                if (gcd(numerator, denominator) == 1) {
+                    result.add(numerator + "/" + denominator);
+                }
+            }
+        }
+        return result;
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
