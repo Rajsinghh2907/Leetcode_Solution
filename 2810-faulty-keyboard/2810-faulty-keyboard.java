280435aade8562e14f0dd04cc036b9c2f5f789f1
Raj Kumar Singh <rajkumarshivam2054@gmail.com>
2024-07-17 23:03:58 +0530

    Time: 5 ms (23.63%), Space: 45.3 MB (13.65%) - LeetHub

--- 2810-faulty-keyboard/2810-faulty-keyboard.java
+++ 2810-faulty-keyboard/2810-faulty-keyboard.java
@@ -1,16 +1,44 @@
 class Solution {
     public String finalString(String s) {
         
+        // StringBuilder sb = new StringBuilder();
+
+        // for(int i = 0; i<s.length(); i++){
+        //     if(s.charAt(i) == 'i'){
+        //         sb.reverse();
+        //     } else{
+        //         sb.append(s.charAt(i));
+        //     }
+        // }
+
+        // return sb.toString();
+
+         
         StringBuilder sb = new StringBuilder();
 
         for(int i = 0; i<s.length(); i++){
             if(s.charAt(i) == 'i'){
-                sb.reverse();
+                sb = new StringBuilder(rev(sb.toString()));
             } else{
                 sb.append(s.charAt(i));
             }
         }
 
         return sb.toString();
+    }
+
+    public static String rev(String st) {
+        char[] charArray = st.toCharArray();
+        int i = 0, j = charArray.length - 1;
+
+        while (i < j) {
+            char temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+            i++;
+            j--;
+        }
+
+        return new String(charArray);
     }
 }
