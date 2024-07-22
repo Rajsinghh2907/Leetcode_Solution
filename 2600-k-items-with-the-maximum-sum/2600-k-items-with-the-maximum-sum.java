c38ab556757cf8a4c24f03eeae74424fe29849db
Raj Kumar Singh <rajkumarshivam2054@gmail.com>
2024-07-22 22:21:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2600-k-items-with-the-maximum-sum/2600-k-items-with-the-maximum-sum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
+        
+        int ans = 0;
+
+        if(numOnes >= k){
+            return k;
+
+        } else if( (numOnes + numZeros) >= k){
+            return numOnes;
+        } else{
+            return numOnes - numNegOnes;
+        }
+
+       
+    }
+}
