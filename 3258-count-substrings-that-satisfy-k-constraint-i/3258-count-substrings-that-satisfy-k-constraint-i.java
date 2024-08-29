13306115669c649618e737294d5f983624e27e53
Raj Kumar Singh <rajkumarshivam2054@gmail.com>
2024-08-30 01:14:39 +0530

    Time: 2 ms (76.86%), Space: 42.4 MB (30.03%) - LeetHub

--- 3258-count-substrings-that-satisfy-k-constraint-i/3258-count-substrings-that-satisfy-k-constraint-i.java
+++ 3258-count-substrings-that-satisfy-k-constraint-i/3258-count-substrings-that-satisfy-k-constraint-i.java
@@ -1,49 +1,82 @@
 class Solution {
     public int countKConstraintSubstrings(String s, int k) {
 
-        int count = 0;
-        int n = s.length();
+
+        // Fully Brute Force
+
+//         int count = 0;
+//         int n = s.length();
         
-        for(int i = 0; i<n; i++){
-            for(int j = i; j<n; j++){
+//         for(int i = 0; i<n; i++){
+//             for(int j = i; j<n; j++){
                 
-                String str = s.substring(i, j+1);
+//                 String str = s.substring(i, j+1);
 
-                int nz = numz(str);
-                int nO = numO(str);
+//                 int nz = numz(str);
+//                 int nO = numO(str);
 
-                if( (nz <=k ) || (nO <= k)){
+//                 if( (nz <=k ) || (nO <= k)){
+//                     count++;
+//                 }
+//             }
+//         }
+
+//         return count;
+//     }
+
+//     // int z = 0;
+//     // int o = 0;
+
+//     public static int numz(String str){
+//         int z = 0;
+//         for(int i = 0; i<str.length(); i++){
+//             if(str.charAt(i) == '0'){
+//                 z++;
+//             }
+//         }
+
+//         return z;
+//     }
+
+//     public static int numO(String str){
+
+//         int o = 0;
+//         for(int i = 0; i<str.length(); i++){
+//             if(str.charAt(i) == '1'){
+//                 o++;
+//             }
+//         }
+
+//         return o;
+//     }
+// }
+
+
+
+
+ int n = s.length();
+        int count = 0;
+        
+        for (int i = 0; i < n; i++) {
+            int zeros = 0;
+            int ones = 0;
+            
+            for (int j = i; j < n; j++) {
+                if (s.charAt(j) == '0') {
+                    zeros++;
+                } else {
+                    ones++;
+                }
+                
+                // Check the k-constraint
+                if (zeros <= k || ones <= k) {
                     count++;
+                } else {
+                    break; // No need to check further as the condition won't hold for longer substrings
                 }
             }
         }
-
+        
         return count;
-    }
-
-    // int z = 0;
-    // int o = 0;
-
-    public static int numz(String str){
-        int z = 0;
-        for(int i = 0; i<str.length(); i++){
-            if(str.charAt(i) == '0'){
-                z++;
-            }
-        }
-
-        return z;
-    }
-
-    public static int numO(String str){
-
-        int o = 0;
-        for(int i = 0; i<str.length(); i++){
-            if(str.charAt(i) == '1'){
-                o++;
-            }
-        }
-
-        return o;
     }
 }
