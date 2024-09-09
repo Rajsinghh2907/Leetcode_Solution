5ff951ac7f85e3ca4efb7ab18d957b525e60cd02
Raj Kumar Singh <rajkumarshivam2054@gmail.com>
2024-09-09 12:37:20 +0530

    Time: 2 ms (29.75%), Space: 41.6 MB (29.57%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -2,28 +2,62 @@ class Solution {
     public int minAddToMakeValid(String s) {
         
 
-        Stack<Character> stack = new Stack<>();
-        int moves = 0; 
+        // Stack<Character> stack = new Stack<>();
+        // int moves = 0; 
 
        
-        for (char c : s.toCharArray()) {
-            if (c == '(') {
+        // for (char c : s.toCharArray()) {
+        //     if (c == '(') {
                
-                stack.push(c);
-            } else {
-                // If it's a closing parenthesis
-                if (!stack.isEmpty() && stack.peek() == '(') {
-                    // If there's a matching opening parenthesis, pop the stack
+        //         stack.push(c);
+        //     } else {
+        //         // If it's a closing parenthesis
+        //         if (!stack.isEmpty() && stack.peek() == '(') {
+        //             // If there's a matching opening parenthesis, pop the stack
+        //             stack.pop();
+        //         } else {
+        //             // If there's no matching opening parenthesis, we need to insert one
+        //             moves++;
+        //         }
+        //     }
+        // }
+
+        // // After the loop, any remaining opening parentheses in the stack need to be closed
+        // // Each unmatched opening parenthesis will need a closing parenthesis
+        // return moves + stack.size();
+
+
+        Stack<Character> stack = new Stack<>();
+        int moves = 0; // Track the number of moves required to make the string valid
+
+        // Iterate through the string character by character
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            // If we encounter a closing parenthesis
+            if (ch == ')') {
+                int count = 0;
+
+                // Pop characters from the stack until we find an opening parenthesis or the stack is empty
+                while (!stack.isEmpty() && stack.peek() != '(') {
                     stack.pop();
-                } else {
-                    // If there's no matching opening parenthesis, we need to insert one
-                    moves++;
+                    count++;
                 }
+
+                // If no matching opening parenthesis was found, it's unbalanced, so we need to add a move
+                if (count == 0 && stack.isEmpty()) {
+                    moves++; // Need to add an opening parenthesis
+                } else {
+                    // Otherwise, we found a matching opening parenthesis, so pop it
+                    if (!stack.isEmpty()) {
+                        stack.pop();
+                    }
+                }
+            } else {
+                // If we encounter an opening parenthesis, push it onto the stack
+                stack.push(ch);
             }
         }
-
-        // After the loop, any remaining opening parentheses in the stack need to be closed
-        // Each unmatched opening parenthesis will need a closing parenthesis
         return moves + stack.size();
     }
 }
