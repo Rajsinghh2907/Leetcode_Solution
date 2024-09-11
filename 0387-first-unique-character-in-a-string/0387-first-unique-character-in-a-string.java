a6f2307175ed813fc297ff9a425023c6cf19d7ac
Raj Kumar Singh <rajkumarshivam2054@gmail.com>
2024-09-11 11:53:50 +0530

    Time: 22 ms (62.42%), Space: 46.6 MB (5.05%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,16 +1,46 @@
 class Solution {
     public int firstUniqChar(String s) {
+
+        //BRUTE FORCE APPROACH
+        
+        // for (int i = 0; i < s.length(); i++) {
+        //     char ch = s.charAt(i);
+        //     String currentChar = String.valueOf(ch);
+
+            
+        //     if (!s.substring(0, i).contains(currentChar) && (!s.substring(i + 1).contains(currentChar) ))  {
+        //         return i; // Return the index of the first non-repeating character
+        //     }
+        // }
+        
+        // return -1; 
+
+
+
+
+        int[] frequency = new int[26]; // For lowercase letters a-z
+        
+        // Queue to store characters and their indices
+        Queue<Integer> queue = new LinkedList<>();
+        
         
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-            String currentChar = String.valueOf(ch);
-
+            frequency[ch - 'a']++;
+            // Add the index of the current character to the queue
+            queue.offer(i);
             
-            if (!s.substring(0, i).contains(currentChar) && (!s.substring(i + 1).contains(currentChar) ))  {
-                return i; // Return the index of the first non-repeating character
+            // Check and remove non-unique characters from the front of the queue
+            while (!queue.isEmpty() && frequency[s.charAt(queue.peek()) - 'a'] > 1) {
+                queue.poll();
             }
         }
         
-        return -1; 
+        // If the queue is not empty, return the index of the first non-repeating character
+        if (!queue.isEmpty()) {
+            return queue.peek();
+        }
+        
+        return -1;
     }
 }
